158e4e409b01be8116808a88dab0a7d55c377598
Dayao359 <yy9907250@163.com>
2025-10-26 22:28:11 +0800

    更新

--- Base/01BaseAuto.py
+++ Base/01BaseAuto.py
@@ -8,14 +8,16 @@ class SettingsAutomation:
     """设置应用自动化类"""
     def __init__(self):
         self.driver = None
-    def setup_driver(self):
+    def setup_driver(self, app_package, app_activity):
         """初始化驱动"""
         options = UiAutomator2Options()
         options.device_name = "PJZ110"
         options.platform_name = "Android"
         options.platform_version = "15"
-        options.app_package = "com.android.settings"
-        options.app_activity = ".Settings"
+        # options.app_package = "com.android.settings"
+        # options.app_activity = ".Settings"
+        options.app_package = app_package
+        options.app_activity = app_activity
         options.no_reset = True
 
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', options=options)
@@ -54,5 +56,23 @@ class SettingsAutomation:
 
 def main():
     auto = SettingsAutomation()
-    auto.setup_driver()
+    damai_package = 'cn.damai'
+    damai_activity = 'cn.damai.homepage.MainActivity'
 
+    try:
+        auto.setup_driver(damai_package,damai_activity)
+        print("大麦APP启动成功！")
+        # print(driver.page_source)
+        auto.click_element(
+            locator="//*[contains(@text,'我的')]",
+            by=AppiumBy.XPATH
+        )
+        print("已点击我的")
+        time.sleep(999)
+    except Exception as e:
+        print(f"运行出错: {str(e)}")
+    finally:
+        auto.quit_driver()
+
+if __name__ == "__main__":
+    main()
